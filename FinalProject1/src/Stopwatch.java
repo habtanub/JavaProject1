cd35dae8cdc0320efaebc7ee4a5ad04962441109
habtanub <105201113+habtanub@users.noreply.github.com>
2022-09-20 23:09:18 +0530

    Add files via upload

---
+++ FinalProject1/src/Stopwatch.java
@@ -0,0 +1,13 @@
+public class Stopwatch extends Timeable{
+    public Stopwatch(Time time) {
+        super(time);
+    }
+
+    /**
+     * Counts the time up.
+     */
+    public void countUp() {
+        // not implemented
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ FinalProject1/src/Timer.java
@@ -0,0 +1,13 @@
+public class Timer extends Timeable{
+    public Timer(Time time) {
+        super(time);
+    }
+
+    /**
+     * Counts down the time.
+     */
+    public void countDown() {
+        // not implemented.
+        throw new UnsupportedOperationException();
+    }
+}
